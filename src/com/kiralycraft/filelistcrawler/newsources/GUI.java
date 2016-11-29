824b686913b8fe9d4aa7eace5c7a24a66f03b0b3
KiralyCraft <alexucu@gmail.com>
2016-11-29 22:42:28 +0200

    Add files via upload

--- src/com/kiralycraft/filelistcrawler/newsources/GUI.java
+++ src/com/kiralycraft/filelistcrawler/newsources/GUI.java
@@ -51,10 +51,10 @@ public class GUI extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		System.setProperty("http.proxyHost", "127.0.0.1");
-	    System.setProperty("https.proxyHost", "127.0.0.1");
-	    System.setProperty("http.proxyPort", "8888");
-	    System.setProperty("https.proxyPort", "8888");
+//		System.setProperty("http.proxyHost", "127.0.0.1");
+//	    System.setProperty("https.proxyHost", "127.0.0.1");
+//	    System.setProperty("http.proxyPort", "8888");
+//	    System.setProperty("https.proxyPort", "8888");
 		try {
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 		} catch (Throwable e) {
